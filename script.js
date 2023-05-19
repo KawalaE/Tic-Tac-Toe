fb1d6a391fc48e643bd89e07c82ddb7dfec979b5
KawalaE <edyta.wer.kawala@gmail.com>
2023-05-19 20:36:28 +0200

    Renders X's and O's on the grid

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const gameBoard = (() =>{
     const board = ['', '','', '','', '','', '',''];
     const gameDisplay = document.getElementById('game-display')
-    board.forEach((element)=>{
+    const render = (marker) => board.forEach((element) =>{
         const boardCube = document.createElement('div');
+        boardCube.textContent = element;
         boardCube.classList.add('cube'); 
         gameDisplay.appendChild(boardCube);
-    })
-    return board;
+        boardCube.addEventListener('click',()=>{
+            console.log('clicked')
+            boardCube.textContent = marker;
+            board[element] = marker;
+            console.log(board)
+        })
+    });
+    return{
+        board,
+        render,
+    }
 })();
 console.log(gameBoard);
+gameBoard.render('O');
+
+function gameLogic(){
+    const xChoice = document.getElementById('X-marker');
+    xChoice.addEventListener('click', ()=>{
+        console.log('X was choosen');
+    });
+};
+gameLogic();
