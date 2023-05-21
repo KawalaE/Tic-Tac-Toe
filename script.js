5151ab7ec12476abcbc75e13c862d6cffdd40b39
KawalaE <edyta.wer.kawala@gmail.com>
2023-05-21 21:08:45 +0200

    Create Player factory function

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const gameBoard = (() =>{
     const board = ['', '','', '','', '','', '',''];
     const gameDisplay = document.getElementById('game-display')
-    const createBoard = (marker) => board.forEach((_element, index) =>{
+    const createBoard = () => board.forEach((_element, index) =>{
         const boardCube = document.createElement('div');
         boardCube.id = index;
         boardCube.classList.add('cube'); 
         gameDisplay.appendChild(boardCube);
-        boardCube.addEventListener('click', markHandler)
+        boardCube.addEventListener('click', markHandler);
     });
     
     return{
@@ -15,9 +15,24 @@ const gameBoard = (() =>{
     }
 })();
 
+let marker = '';
 function markHandler(e){
     const displayMark = document.createElement('div');
-    displayMark.classList.add('cross');
+    displayMark.classList.add(marker);
     e.target.append(displayMark);
 }
 gameBoard.createBoard();
+
+const Player = (mark) =>{
+    const setMarker = () =>{
+        marker = mark;
+    }
+    return {setMarker, mark}
+};
+
+const gameLogic = (() => {
+    const playerOne = Player('cross');
+    const playerTwo = Player('circle');
+    playerOne.setMarker();
+    //playerOne.mark === 'cross' ? playerTwo.setMarker() : playerOne.setMarker();
+})();
