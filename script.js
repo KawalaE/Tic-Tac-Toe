bb8f1dbf20439ba55039aa34943e471ccef73121
KawalaE <edyta.wer.kawala@gmail.com>
2023-05-22 19:42:21 +0200

    Add game status info

--- script.js
+++ script.js
@@ -1,38 +1,89 @@
-/**gameboard module ->  board render function */
+let gameStatus = document.querySelector('.status');
+
 const gameBoard = (() =>{
     const board = ['', '','', '','', '','', '',''];
-    const gameDisplay = document.getElementById('game-display')
+    const gameDisplay = document.getElementById('game-display');
+    const restartGame = document.querySelector('.restart');
     const createBoard = () => board.forEach((_element, index) =>{
         const boardCube = document.createElement('div');
-        boardCube.id = index;
+        boardCube.id = index+1;
         boardCube.classList.add('cube'); 
         gameDisplay.appendChild(boardCube);
         boardCube.addEventListener('click', markHandler);
     });
+    restartGame.addEventListener('click', clearGameBoard);
     
     return{
-        createBoard,
+        createBoard
     }
 })();
 
+function clearGameBoard(){
+    document.querySelectorAll('.cube').forEach((cube)=>{
+        if(cube.firstChild){
+            cube.removeChild(cube.firstChild);
+        }
+        cube.addEventListener('click', markHandler)
+        
+    })
+}
 let marker = '';
 function markHandler(e){
     const displayMark = document.createElement('div');
     displayMark.classList.add(marker);
     e.target.append(displayMark);
+    marker ==='cross' ? playerTwo.setMarker() : playerOne.setMarker();
+    e.target.removeEventListener('click', markHandler);
+    checkScore();
 }
 gameBoard.createBoard();
 
+function checkScore(){
+    const allCrosses = document.querySelectorAll('.cross');
+    const allCircles = document.querySelectorAll('.circle');
+    let crossMoves = [];
+    let circleMoves = [];
+    allCrosses.forEach((cross) =>{
+        crossMoves.push(Number(cross.parentNode.id));
+    });
+    allCircles.forEach((circle)=>{
+        circleMoves.push(Number(circle.parentNode.id))
+    })
+    const winningCombos = [
+        [1,2,3], [4,5,6], [7,8,9],
+        [1,4,7], [2,5,8], [3,6,9],
+        [1,5,9], [3,5,7]
+    ]; 
+
+    winningCombos.forEach((winArray) =>{
+        if(isSubset(crossMoves, winArray)){
+            gameStatus.textContent = "X wins!";
+            gamePause();
+            
+        }else if(isSubset(circleMoves, winArray)){
+            gameStatus.textContent = "O wins!";
+            gamePause()
+        }
+    })
+}
+function isSubset(mainArr, subArray){
+    for (let el in subArray){
+        if (!mainArr.includes(subArray[el])){
+            return false;
+        }
+    } return true;
+}
+function gamePause(){
+    document.querySelectorAll('.cube').forEach((cube)=>{
+        cube.removeEventListener('click', markHandler);
+    })
+}
 const Player = (mark) =>{
     const setMarker = () =>{
         marker = mark;
     }
     return {setMarker, mark}
 };
-
-const gameLogic = (() => {
-    const playerOne = Player('cross');
-    const playerTwo = Player('circle');
-    playerOne.setMarker();
-    //playerOne.mark === 'cross' ? playerTwo.setMarker() : playerOne.setMarker();
-})();
+const playerOne = Player('cross');
+const playerTwo = Player('circle');
+playerOne.setMarker();
